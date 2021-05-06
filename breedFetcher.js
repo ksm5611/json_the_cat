f57d7beba7973518a11d02cd563c2e49b226dcc2
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-06 23:09:38 +0000

    so hard to understand

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,24 @@
-const fs  = require('fs');
+// const fs  = require('fs');
 const request = require('request');
-const yargs = require('yargs').argv;
+// const yargs = require('yargs').argv;
 // console.log(yargs._[0]);
-const catBreed = yargs._[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-  // console.log(response);
-  // console.log(typeof body);
-  // console.log(body)
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
-  if (error) {
-    console.log('Breed Not Found');
-    return;
-  }
-  fs.writeFile(catBreed, body, function(error) {
-    if (error) {
-      console.log('Request Failed');
-      return;
+
+const fetchBreedDescription = function(breedName, callback) {
+  console.log("name", breedName);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    let data = null;
+
+    if (!error) {
+      if (JSON.parse(body).length === 0) {
+        error = 'invalid breed name';
+      } else {
+        data = JSON.parse(body)[0]['description'];
+      }
     }
+    callback(error, data);
+    
   });
-});
+};
+
+module.exports = { fetchBreedDescription };
